dc73be237ac185257570046f9325d705b51cc6ca
P3rseo <115739337+P3rseo@users.noreply.github.com>
2025-02-18 09:51:14 +0100

    Proyecto base
    Comienzo

---
+++ src/main/java/es/museotrapo/trapo/TrapoApplication.java
@@ -0,0 +1,13 @@
+package es.museotrapo.trapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/museotrapo/trapo/TrapoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.museotrapo.trapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
