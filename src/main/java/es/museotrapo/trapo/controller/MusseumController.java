6dd89a3e21ea49a0913291e03f36e5aa8f605575
asaanchezz <a.sanchezdiaz.5@gmail.com>
2025-02-25 10:45:06 +0100

    Cambio de controller a service

--- src/main/java/es/museotrapo/trapo/controller/MusseumController.java
+++ src/main/java/es/museotrapo/trapo/controller/MusseumController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import es.museotrapo.trapo.model.Artist;
 import es.museotrapo.trapo.model.Picture;
 import es.museotrapo.trapo.model.Ticket;
+import es.museotrapo.trapo.service.MusseumService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,25 +16,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MusseumController {
     
-    private List<Picture> pictures = new ArrayList<>();
-    private List<Artist> artists = new ArrayList<>();
-    private List<Ticket> boughtTickets = new ArrayList<>();
+
+    private final MusseumService service;
+
+    public MusseumController(MusseumService service) {
+        this.service = service;
+    }
 
     @GetMapping("/")
     public String showAll(Model model){
-
-        model.addAttribute("pictures", pictures);
-        model.addAttribute("artists", artists);
+        model.addAttribute("pictures", service.getPictures());
+        model.addAttribute("artists", service.getArtists());
 
         return "index";
     }
 
     @PostMapping("/pictures/predefined")
-    public String predefinedPictures(Model model){
-        
-        pictures.add(new Picture("La Mona Lisa", "10 02 03", "https://th.bing.com/th/id/OIP.TPkYW_eMIbYQekiVd8_p6gAAAA?w=191&h=215&c=7&r=0&o=5&pid=1.7"));
-        pictures.add(new Picture("La Mona Tiesa", "10 02 03", "https://th.bing.com/th?id=OIP.jOsfhX9fuPkNwu2SlzR4fgHaJ8&w=215&h=289&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"));
-        pictures.add(new Picture("La Mona Calva", "10 02 03", "https://th.bing.com/th?id=OIP.pJsoD_jzIuo83_6Tj-RiwgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"));
+    public String predefinedPictures(Model model){;
+        service.getPictures().add(new Picture("La Mona Lisa", "10 02 03", "https://th.bing.com/th/id/OIP.TPkYW_eMIbYQekiVd8_p6gAAAA?w=191&h=215&c=7&r=0&o=5&pid=1.7"));
+        service.getPictures().add(new Picture("La Mona Tiesa", "10 02 03", "https://th.bing.com/th?id=OIP.jOsfhX9fuPkNwu2SlzR4fgHaJ8&w=215&h=289&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"));
+        service.getPictures().add(new Picture("La Mona Calva", "10 02 03", "https://th.bing.com/th?id=OIP.pJsoD_jzIuo83_6Tj-RiwgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"));
 
         return "redirect:/";
     }
@@ -41,9 +43,9 @@ public class MusseumController {
     @PostMapping("/artists/predefined")
     public String predefinedArtists(Model model){
 
-        artists.add(new Artist("Samuel Lopez", "DjSamsa", "No he nacido todavia"));
-        artists.add(new Artist("Alejandro Sanchez", "AlexanderMaqueeen", "No he nacido todavia"));
-        artists.add(new Artist("EL Random que se ha unido", "PutoRandom", "No he nacido todavia"));
+        service.getArtists().add(new Artist("Samuel Lopez", "DjSamsa", "No he nacido todavia"));
+        service.getArtists().add(new Artist("Alejandro Sanchez", "AlexanderMaqueeen", "No he nacido todavia"));
+        service.getArtists().add(new Artist("EL Random que se ha unido", "PutoRandom", "No he nacido todavia"));
         
         return "redirect:/";
     }
@@ -51,7 +53,7 @@ public class MusseumController {
     @PostMapping("/picture/new")
     public String newPicture(Model model, Picture picture){
         
-        pictures.add(picture);
+        service.getPictures().add(picture);
 
         return "saved_picture";
     }
@@ -59,7 +61,7 @@ public class MusseumController {
     @PostMapping("/artist/new")
     public String newArtist(Model model, Artist artist){
 
-        artists.add(artist);
+        service.getArtists().add(artist);
 
         return "saved_artist";
     }
@@ -67,7 +69,7 @@ public class MusseumController {
     @PostMapping("/ticket/new")
     public String newTicket(Model model, Ticket ticket){
 
-        boughtTickets.add(ticket);
+        service.getBoughtTickets().add(ticket);
 
         return "saved_ticket";
     }
@@ -75,7 +77,7 @@ public class MusseumController {
     @GetMapping("pictures/{numPicture}")
     public String showPicture(Model model, @PathVariable int numPicture){
 
-        Picture picture = pictures.get(numPicture - 1);
+        Picture picture = service.getPictures().get(numPicture - 1);
         model.addAttribute("picture", picture);
         model.addAttribute("numPicture", numPicture);
 
@@ -85,7 +87,7 @@ public class MusseumController {
     @GetMapping("artists/{numArtist}")
     public String showArtist(Model model, @PathVariable int numArtist){
 
-        Artist artist = artists.get(numArtist - 1);
+        Artist artist = service.getArtists().get(numArtist - 1);
         model.addAttribute("artist", artist);
         model.addAttribute("numArtist", numArtist);
 
@@ -95,7 +97,7 @@ public class MusseumController {
     @PostMapping("/picture/{numPicture}/delete")
     public String deletePicture(Model model, @PathVariable int numPicture){
 
-        pictures.remove(numPicture - 1);
+        service.getPictures().remove(numPicture - 1);
         model.addAttribute("numPicture", numPicture);        
         
         return "deleted_picture";
@@ -104,7 +106,7 @@ public class MusseumController {
     @PostMapping("/artist/{numArtist}/delete")
     public String deleteArtist(Model model, @PathVariable int numArtist){
 
-        artists.remove(numArtist - 1);
+        service.getArtists().remove(numArtist - 1);
         model.addAttribute("numArtist", numArtist);        
         
         return "deleted_artist";
@@ -113,7 +115,7 @@ public class MusseumController {
     @PostMapping("/picture/{numPicture}/like")
     public String likePicture(Model model, @PathVariable int numPicture){
 
-        Picture picture = pictures.get(numPicture - 1);
+        Picture picture = service.getPictures().get(numPicture - 1);
         //picture.giveLike(Member); Un metodo en el que se añade el like a la lista de likes del cuadro y se añade el cuadro a la lista de Member.
         //picture.quitLike(Member) lo mismo pero quitando el like.
         model.addAttribute("picture", picture);
--- src/main/java/es/museotrapo/trapo/service/MusseumService.java
+++ src/main/java/es/museotrapo/trapo/service/MusseumService.java
@@ -1,8 +1,66 @@
 package es.museotrapo.trapo.service;
 
+import es.museotrapo.trapo.model.Artist;
+import es.museotrapo.trapo.model.Member;
+import es.museotrapo.trapo.model.Picture;
+import es.museotrapo.trapo.model.Ticket;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MusseumService {
-    
-    // Aqui vamos a meter las listas en vez de tenerlas en el controlador.
 
+    private List<Picture> pictures = new ArrayList<>();
+    private List<Artist> artists = new ArrayList<>();
+    private List<Ticket> boughtTickets = new ArrayList<>();
 
+    public List<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(List<Artist> artists) {
+        this.artists = artists;
+    }
+
+    public List<Ticket> getBoughtTickets() {
+        return boughtTickets;
+    }
+
+    public void setBoughtTickets(List<Ticket> boughtTickets) {
+        this.boughtTickets = boughtTickets;
+    }
+
+    public void giveLikeTo(Member member, Picture picture){
+        List<Member> newMemberLike = picture.getMemberLikes();
+        List<Picture> newPictureLike = member.getLikedPictures();
+        if(!newMemberLike.contains(member) && !newPictureLike.contains(picture)) {
+            newMemberLike.add(member);
+            newPictureLike.add(picture);
+        }
+        picture.setMemberLikes(newMemberLike);
+        member.setLikedPictures(newPictureLike);
+    }
+
+/*
+    public void purchaseTickets(Member member, String date){
+        Ticket ticket;
+        ticket = new Ticket(date, member);
+        ticket.setOwner(member);
+        member.setTicket(true);
+    }
+
+    public boolean hasTicket(Member member){
+        return member.isTicket();
+    }
+*/
 }
