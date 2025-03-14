4083cb1d44561d3d42716bb9cca0692a65caf158
P3rseo <115739337+P3rseo@users.noreply.github.com>
2025-03-14 11:05:09 +0100

    coooooomit

--- src/main/java/es/museotrapo/trapo/model/Artist.java
+++ src/main/java/es/museotrapo/trapo/model/Artist.java
@@ -3,9 +3,19 @@ package es.museotrapo.trapo.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Artist {
 
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String name;
     private String nickname;
     private String birthDate;
--- src/main/java/es/museotrapo/trapo/model/Comment.java
+++ src/main/java/es/museotrapo/trapo/model/Comment.java
@@ -1,8 +1,18 @@
 package es.museotrapo.trapo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Comment {
 
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String message;
     private User author;
 
