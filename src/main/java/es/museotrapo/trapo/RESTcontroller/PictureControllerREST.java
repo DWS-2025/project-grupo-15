bb20be97e0eee55ef50dd92acdae325c3c141f5c
P3rseo <115739337+P3rseo@users.noreply.github.com>
2025-04-01 10:40:03 +0200

    PictureControllerRest

--- src/main/java/es/museotrapo/trapo/RESTcontroller/PictureControllerREST.java
+++ src/main/java/es/museotrapo/trapo/RESTcontroller/PictureControllerREST.java
@@ -3,8 +3,14 @@ package es.museotrapo.trapo.RESTcontroller;
 import es.museotrapo.trapo.dto.PictureDTO;
 import es.museotrapo.trapo.service.PictureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
 
 
@@ -26,6 +32,16 @@ public class PictureControllerREST {
     public PictureDTO getPicture(@PathVariable long id) {
 
         return pictureService.getPicture(id);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<PictureDTO> createArtist(@RequestBody PictureDTO PictureDTO, Long artistId, MultipartFile imageFile) throws IOException{
+        
+        PictureDTO = pictureService.createPicture(PictureDTO, artistId, imageFile);
+
+        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(PictureDTO.id()).toUri();
+        
+        return ResponseEntity.created(location).body(PictureDTO);
     }
 
     @DeleteMapping("/{id}")
